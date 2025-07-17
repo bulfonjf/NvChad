96eb70cfec86c9a54810dc3046d12288ed87699e
jbulfon <jbulfon@ondiss.com.ar>
2025-07-17 12:33:03 +0000

    Comment format on save for conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -14,11 +14,11 @@ local options = {
     cs = { "csharpier" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 5500,
+  --   lsp_fallback = false,
+  -- },
 }
 
 return options
