a5ef38114416b89ec6334e338cd70acea90931bc
jbulfon <jbulfon@ondiss.com.ar>
2025-07-17 12:02:29 +0000

    LspConfig updated

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,46 +2,40 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").oninit
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tailwindcss" }
+local servers = { "html", "cssls", "tailwindcss", "lua_ls" }
 --local util = require "lspconfig/util"
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
+vim.lsp.enable(servers)
 
 -- Typescript
 lspconfig.ts_ls.setup {
   on_init = on_init,
   capabilities = capabilities,
-  on_attach = function(_, bufnr)
-    -- Set up formatting or organize imports on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.code_action {
-          context = {
-            only = { "source.removeUnusedImports" },
-            diagnostics = {},
-          },
-          apply = true,
-        }
-
-        vim.cmd "sleep 300m" -- Sleep for 300 milliseconds
-
-        vim.lsp.buf.code_action {
-          context = {
-            only = { "source.organizeImports" },
-            diagnostics = {},
-          },
-          apply = true,
-        }
-      end,
-    })
-  end,
+  on_attach = on_attach,
+  -- on_attach = function(_, bufnr)
+  --   -- Set up formatting or organize imports on save
+  --   vim.api.nvim_create_autocmd("BufWritePre", {
+  --     buffer = bufnr,
+  --     callback = function()
+  --       vim.lsp.buf.code_action {
+  --         context = {
+  --           only = { "source.removeUnusedImports" },
+  --           diagnostics = {},
+  --         },
+  --         apply = true,
+  --       }
+  --
+  --       vim.cmd "sleep 300m" -- Sleep for 300 milliseconds
+  --
+  --       vim.lsp.buf.code_action {
+  --         context = {
+  --           only = { "source.organizeImports" },
+  --           diagnostics = {},
+  --         },
+  --         apply = true,
+  --       }
+  --     end,
+  --   })
+  -- end,
 }
 
 -- Eslint-lsp
