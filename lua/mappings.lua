caeec4b80fac7be64c94f1e4b95deb607effb127
jbulfon <jbulfon@ondiss.com.ar>
2025-07-17 12:32:42 +0000

    Put descriptions on all mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,14 +11,14 @@ map("i", "jk", "<ESC>")
 
 -- === LSP (nvim-lspconfig + nvim-cmp) ===
 -- Go to definitions, references, hover, etc
-map("n", "gd", vim.lsp.buf.definition, opts)
-map("n", "gr", vim.lsp.buf.references, opts)
-map("n", "K", vim.lsp.buf.hover, opts)
-map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+map("n", "gd", vim.lsp.buf.definition, {silent=true, noremap=true, desc = "Go to Definition"})
+map("n", "gr", vim.lsp.buf.references, {silent=true, noremap=true, desc = "Go to References"})
+map("n", "K", vim.lsp.buf.hover, {silent=true, noremap=true, desc = "Hover Documentation"})
+map("n", "<leader>rn", vim.lsp.buf.rename, {silent=true, noremap=true, desc = "Rename Symbol"})
+map("n", "<leader>ca", vim.lsp.buf.code_action, {silent=true, noremap=true, desc = "Code Actions"})
 
 -- Diagnostics
-map("n", "<leader>d", vim.diagnostic.open_float, opts)
+map("n", "<leader>d", vim.diagnostic.open_float, {silent=true, noremap=true, desc = "Open Diagnostics Float"})
 
 -- === Completion (nvim-cmp) ===
 -- Usually mapped inside cmp config, but these help:
@@ -29,28 +29,28 @@ end, { noremap = true, silent = true })
 -- === Debugging (nvim-dap + nvim-dap-ui) ===
 map("n", "<F5>", function()
   require("dap").continue()
-end, opts)
+end, { silent = true, noremap = true, desc = "Start/Continue Debugging" })
 map("n", "<F10>", function()
   require("dap").step_over()
-end, opts)
+end, { silent = true, noremap = true, desc = "Step Over" })
 map("n", "<F11>", function()
   require("dap").step_into()
-end, opts)
+end, { silent = true, noremap = true, desc = "Step Into" })
 map("n", "<F12>", function()
   require("dap").step_out()
-end, opts)
+end, { silent = true, noremap = true, desc = "Step Out" })
 map("n", "<leader>b", function()
   require("dap").toggle_breakpoint()
-end, opts)
+end, { silent = true, noremap = true, desc = "Toggle Breakpoint" })
 map("n", "<leader>B", function()
   require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ")
-end, opts)
+end, { silent = true, noremap = true, desc = "Set Conditional Breakpoint" })
 map("n", "<leader>dr", function()
   require("dap").repl.open()
-end, opts)
+end, { silent = true, noremap = true, desc = "Open Debug REPL" })
 map("n", "<leader>du", function()
   require("dapui").toggle()
-end, opts)
+end, { silent = true, noremap = true, desc = "Toggle Debug UI" })
 
 -- === Git (gitsigns.nvim + fugitive + diffview.nvim + git-conflict.nvim) ===
 -- Navigation
@@ -71,20 +71,19 @@ map("n", "[c", function()
 end, { expr = true })
 
 -- Hunk actions
---map("n", "<leader>hl", gitsigns.stage_hunk, opts)
-map("n", "<leader>hs", gitsigns.stage_hunk, opts)
-map("n", "<leader>hr", gitsigns.reset_hunk, opts)
-map("n", "<leader>hS", gitsigns.stage_buffer, opts)
-map("n", "<leader>hu", gitsigns.undo_stage_hunk, opts)
-map("n", "<leader>hR", gitsigns.reset_buffer, opts)
-map("n", "<leader>hp", gitsigns.preview_hunk, opts)
+map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
+map("n", "<leader>hS", ":Gitsigns stage_buffer<CR>", opts)
+map("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>", opts)
+map("n", "<leader>hR", ":Gitsigns reset_buffer<CR>", opts)
+map("n", "<leader>hp", ":Gitsigns preview_hunk<CR>", opts)
 map("n", "<leader>hb", function()
-  gitsigns.blame_line { full = true }
-end, opts)
-map("n", "<leader>hD", gitsigns.diffthis, opts)
+  blame_line { full = true }
+end, { silent = true, noremap = true, desc = "Git Blame Line" })
+map("n", "<leader>hD", ":Gitsigns diffthis<CR>", opts)
 map("n", "<leader>hF", function()
   gitsigns.diffthis "~"
-end, opts)
+end, { silent = true, noremap = true, desc = "Git Diff with HEAD" })
 
 map("n", "<leader>cc", function()
   local message = vim.fn.input("Commit message: ", "")
@@ -96,17 +95,18 @@ map("n", "<leader>cc", function()
 end, { noremap = true, silent = true, desc = "Git Commit with Message" })
 
 -- Toggles
-map("n", "<leader>tb", gitsigns.toggle_current_line_blame, opts)
-map("n", "<leader>td", gitsigns.toggle_deleted, opts)
+map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { silent = true, noremap = true, desc = "Toggle Line Git Blame" })
+map("n", "<leader>td", gitsigns.toggle_deleted, { silent = true, noremap = true, desc = "Toggle Deleted Lines" })
+map("n", "<leader>ts", gitsigns.toggle_signs, { silent = true, noremap = true, desc = "Toggle Git Signs" })
 
 -- Visual mode mappings
 map("v", "<leader>hs", function()
   gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-end, opts)
+end, { silent = true, noremap = true, desc = "Stage Visual Hunk" })
 
 map("v", "<leader>hr", function()
   gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-end, opts)
+end, { silent = true, noremap = true, desc = "Reset Visual Hunk" })
 
 -- === Diffview ===
 map("n", "<leader>gd", ":DiffviewOpen<CR>", opts)
@@ -121,53 +121,73 @@ map(
   "n",
   "<leader>xx",
   "<cmd>Trouble diagnostics toggle <cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "Diagnostics (Trouble)" })
+  { silent = true, noremap = true, desc = "Diagnostics (Trouble) All" }
 )
 
 map(
   "n",
   "<leader>xX",
   "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "Buffer Diagnostics (Trouble)" })
+  { silent = true, noremap = true, desc = "Diagnostics (Trouble) - Current Buffers" }
 )
 
 map(
   "n",
   "<leader>cs",
   "<cmd>Trouble symbols toggle focus=false win.size=0.4<cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "Symbols (Trouble)" })
+  { silent = true, noremap = true, desc = "Symbols (Trouble)" }
 )
 
 map(
   "n",
   "<leader>cl",
   "<cmd>Trouble lsp toggle focus=true win.position=right win.size=0.4 <cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "LSP Definitions / references / ... (Trouble)" })
+  { silent = true, noremap = true, desc = "LSP (Trouble)" }
 )
 
 map(
   "n",
   "<leader>xL",
   "<cmd>Trouble loclist toggle<cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "Location List (Trouble)" })
+  { silent = true, noremap = true, desc = "Location List (Trouble)" }
 )
 
 map(
   "n",
   "<leader>xQ",
   "<cmd>Trouble qflist toggle<cr>",
-  vim.tbl_deep_extend("force", opts, { desc = "Quickfix List (Trouble)" })
+  { silent = true, noremap = true, desc = "Quickfix List (Trouble)" }
 )
 
 -- === Buffers ===
-map("n", "<leader>bc", ":%bd!|e#|bd!#<CR>", opts)
+map("n", "<leader>bc", ":%bd!|e#|bd!#<CR>", {silent=true, noremap=true, desc = "Close all buffers except current"})
 
 -- === AI assistant (codeium.nvim) ===
 
 -- === Misc ===
 -- Toggle NvimTree (if you use it)
 --map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+--
+
+-- Global toggle variable
+vim.g.diagnostics_visible = true
+
+-- Toggle function
+function _G.toggle_diagnostics()
+  vim.g.diagnostics_visible = not vim.g.diagnostics_visible
+  if vim.g.diagnostics_visible then
+    vim.diagnostic.show()
+    print("Diagnostics: ON")
+  else
+    vim.diagnostic.hide()
+    print("Diagnostics: OFF")
+  end
+end
+
+-- Example mapping: <leader>td to toggle diagnostics
+map("n", "<leader>tx", toggle_diagnostics, {silent=true, noremap=true, desc = "Toggle Diagnostics" })
+
 
 -- Quick save
-map("n", "<leader>w", ":w<CR>", opts)
-map("n", "<leader>q", ":q<CR>", opts)
+map("n", "<leader>w", ":w<CR>", { silent = true, noremap = true, desc = "Save File" })
+map("n", "<leader>q", ":q<CR>", { silent = true, noremap = true, desc = "Quit" })
